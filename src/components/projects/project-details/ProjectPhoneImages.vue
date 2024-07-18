1df1b9ff0ab25ee8ca053e58a4dd6228cb61e775
Thanasis Traitsis <thanasiswd@gmail.com>
2024-07-18 16:13:22 +0300

    remove images animation

--- src/components/projects/project-details/ProjectPhoneImages.vue
+++ src/components/projects/project-details/ProjectPhoneImages.vue
@@ -1,56 +1,7 @@
 <script setup>
   import { defineProps, onMounted, watch } from "vue";
-  import gsap from "gsap";
-  import ScrollTrigger from "gsap/ScrollTrigger";
-  gsap.registerPlugin(ScrollTrigger);
 
   const { phoneImages } = defineProps(["phoneImages"]);
-
-  onMounted(() => {
-    let mm = gsap.matchMedia();
-
-    mm.add("(min-width: 1290px)", () => {
-      const elements = document.querySelectorAll(".project_images div");
-
-      for (let i = 0; i < elements.length; i += 3) {
-        const el1 = elements[i];
-        const el2 = elements[i + 1];
-        const el3 = elements[i + 2];
-        const elementsToAnimate = [el1, el2, el3].filter(Boolean);
-
-        gsap
-          .timeline({
-            scrollTrigger: {
-              trigger: elementsToAnimate[0],
-              start: "top 50%",
-              end: "bottom 90%",
-              scrub: true,
-            },
-          })
-          .fromTo(elementsToAnimate[0], { x: "-50vw" }, { x: 0, duration: 2 })
-          .fromTo(
-            elementsToAnimate[2],
-            { x: "50vw" },
-            { x: 0, duration: 2 },
-            "<"
-          );
-      }
-
-      if (elements.length % 3 !== 0) {
-        const lastElement = elements[elements.length - (elements.length % 3)];
-        gsap
-          .timeline({
-            scrollTrigger: {
-              trigger: lastElement,
-              start: "top 50%",
-              end: "bottom 20%",
-              scrub: true,
-            },
-          })
-          .fromTo(lastElement, { x: "-50vw" }, { x: 0, duration: 1 });
-      }
-    });
-  });
 </script>
 
 <template>
